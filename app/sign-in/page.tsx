f12538106a29a49194c178b07786ff87a1f28367
DaViD aLsAbRoOk <dfalsabrook@gmail.com>
2025-03-20 03:45:24 +0000

    forgot to change the github button

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
               <form
                 action={async () => {
                   "use server";
-                  await signIn("default", { redirectTo: "/" });
+                  await signIn("github", { redirectTo: "/" });
                 }}
               >
                 <button className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50">
